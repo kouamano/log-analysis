9a5a21046a69f32f26f7fc91e6f33d4533edea99
kouamano <amano.au1@gmail.com>
2024-01-12 14:17:50 +0000

    add

---
+++ apply_code_ALL.sh
@@ -0,0 +1,6 @@
+/home/amano/gitsrc/log-analysis/D_archive/apply_code_archive.sh
+/home/amano/gitsrc/log-analysis/D_base/apply_code_base.sh
+/home/amano/gitsrc/log-analysis/D_copy/apply_code_copy.sh
+/home/amano/gitsrc/log-analysis/D_exec/apply_code_exec.sh
+/home/amano/gitsrc/log-analysis/D_put/apply_code_put.sh
+/home/amano/gitsrc/log-analysis/voc/apply_code_cov.sh
