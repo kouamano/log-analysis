d033d5a0708070168d3cb6e8182fe039ad6ede59
kouamano <amano.au1@gmail.com>
2024-08-28 00:44:38 +0000

    new

---
+++ D_put/putAllGraphToHDFS_5da.d
@@ -0,0 +1,10 @@
+basedir=/home/jovyan/usr/amano/notebooks/data/rcoslogs/log
+targetday=`date -d '5 day ago' '+%Y%m%d'`
+#mkdir -p ${basedir}/D=$targetday
+cd ${basedir}/D=${targetday}
+pwd
+ls
+hdfs dfs -put T=ci/ckanedgelist.${targetday}.json log-graph/daily/ci/ckanedgelist.${targetday}.json
+hdfs dfs -put T=4/_story/edgelist-4.${targetday}.json log-graph/daily/all/edgelist-4.${targetday}.json
+#hdfs dfs -copyToLocal /user/rcosloguser02/logs/log-${targetday}.rcoslog000[123].log ./
+#chmod -R 777 ${basedir}/D=$targetday
