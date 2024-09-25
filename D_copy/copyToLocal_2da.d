2f42e3c0d4342f93091a1a42f5008b675b89ff26
kouamano <amano.au1@gmail.com>
2024-09-25 08:13:51 +0000

    rev

--- D_copy/copyToLocal_2da.d
+++ D_copy/copyToLocal_2da.d
@@ -2,5 +2,5 @@ basedir=/home/jovyan/usr/amano/notebooks/data/rcoslogs/log
 targetday=`date -d '2 day ago' '+%Y%m%d'`
 mkdir -p ${basedir}/D=$targetday
 cd ${basedir}/D=$targetday
-hdfs dfs -copyToLocal /user/rcoslog/logs/log-${targetday}.rcoslog000[123].log ./
+hdfs dfs -copyToLocal /user/rcoslog/logs/log-${targetday}.rcoslog*.log ./
 chmod -R 777 ${basedir}/D=$targetday
