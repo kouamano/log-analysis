421a175fb51aa47a95782863d4af410bf38ac419
kouamano <amano.au1@gmail.com>
2024-09-26 01:13:17 +0000

    rev

--- D_copy/copyToLocal_2da.d
+++ D_copy/copyToLocal_2da.d
@@ -2,5 +2,13 @@ basedir=/home/jovyan/usr/amano/notebooks/data/rcoslogs/log
 targetday=`date -d '2 day ago' '+%Y%m%d'`
 mkdir -p ${basedir}/D=$targetday
 cd ${basedir}/D=$targetday
-hdfs dfs -copyToLocal /user/rcoslog/logs/log-${targetday}.rcoslog*.log ./
+str=`hdfs dfs -copyToLocal /user/rcoslog/logs/log-${targetday}.rcoslog*.log ./ 2>&1`
 chmod -R 777 ${basedir}/D=$targetday
+
+ret=0
+if [[ $str =~ "No such" ]]; then
+  echo "source file does not exist"
+  ret=1
+fi
+
+exit $ret
